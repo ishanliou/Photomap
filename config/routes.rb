e2d34e007630b09079c134be810f8f9ea12d9072
Ishan <ishanliou@yahoo.com>
2018-01-10 17:19:37 -0800

    og

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
  root 'users#index'
  resources :users
- get '/logout' => 'sessions#destroy', as: :logout
+ delete '/logout' => 'sessions#destroy', as: :logout
  resources :sessions, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
