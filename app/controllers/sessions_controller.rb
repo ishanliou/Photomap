6c0af25ca312316f8368ddf677974447b6e2221c
Ishan <ishanliou@yahoo.com>
2018-01-10 18:35:20 -0800

    fixed destroy routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,8 +14,7 @@ class SessionsController < ApplicationController
   end
 
   #logout
-  def destroy
-    
+  def destroy 
     session[:user_id] = nil
     redirect_to root_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
  root 'users#index'
  resources :users
- delete '/logout' => 'sessions#destroy', as: :logout
  resources :sessions, only: [:new, :create]
+ get '/logout' => 'sessions#destroy', as: :logout
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
